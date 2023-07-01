a60df17bef010c190d69ea6b683affe35f48a931
kirillof <113895165+kkirillof@users.noreply.github.com>
2023-07-01 15:37:47 +0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('myInput');
-const button = document.getElementById('myButton');
-
-
-button.addEventListener('click', sayhi);
-
-function sayhi() {
-    alert(`Привет, ${input.value}! Как твои дела?`);
-}
