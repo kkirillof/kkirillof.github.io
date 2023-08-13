c2745aa46935fddf57760db4667f78402ed473c7
kirillof <113895165+kkirillof@users.noreply.github.com>
2023-08-13 21:46:32 +0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var i1 = document.getElementById("i1");
-
-// Добавляем обработчик события клика на блок
-i1.addEventListener("click", function() {
-  // Переходим по ссылке
-  window.open("https://www.avito.ru/astrahan/chasy_i_ukrasheniya/casio_g-shock_serye_3124500435", "_blank");
-});
